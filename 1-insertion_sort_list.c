799a0c06f1eb45fc4107b161f9e15bb7d1b4ce85
TessierV <5628@holbertonstudents.com>
2023-01-02 09:01:29 -0800

    switch area for null handle and betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,17 +1,18 @@
 #include "sort.h"
 
-/*
- * insertion_sort_list - sorts a doublylinked int in ascending using the Insertion sort algo
+/**
+ * insertion_sort_list - sorts a doublylinked using the Insertion sort algo
  * @list: list of int
  */
 
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *newNode;
+
+	if (!list || !(*list)->next)
+		return;
 	newNode = (*list)->next;
 
-	if (!list || !(*list))
-		return;
 	while (newNode)
 	{
 		while (newNode->prev && newNode->n < newNode->prev->n)
@@ -24,7 +25,6 @@ void insertion_sort_list(listint_t **list)
 			if (newNode->prev)
 				newNode->prev->next = newNode;
 			newNode->next->prev = newNode;
-			
 			if (!newNode->prev)
 				*list = newNode;
 			print_list(*list);
