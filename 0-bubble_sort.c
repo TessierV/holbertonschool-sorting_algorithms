57e061c6a714d441cb625dbe03b9a7959da94951
MoeAlAnnan <5653@holbertonstudents.com>
2023-01-03 01:56:15 -0800

    fixing 0-O and bubble sort betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,26 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - function
+ * @array: array to be sorted
+ * @size: size of the array
+ * Definition: a function that sorts an array
+ * Return: void
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	i = 0;
-	while (i < size)
+	for (i = 0; i < size - 1; i++)
 	{
-		j = 0;
-		while (j < size - 1)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j+1];
-				array[j + 1] = temp;
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
 				print_array(array, size);
 			}
-			j++;
 		}
-		i++;
 	}
 }
